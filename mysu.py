01cef55c29b5da7adc51135819d7909ca43704c9
Ivan Kirillov <vayw@botans.org>
2015-07-17 21:06:45 +0300

    initial commit

---
+++ mysu.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import subprocess
+from time import strftime, localtime
+import uuid
+
+# function which generates pretty unique filename
+def mkname():
+    # localtime presented in format "%Y%m%d%M%S"
+    a_part = strftime("%Y%m%d%M%S", localtime())
+    # random 32-character hexadecimal string
+    b_part = uuid.uuid4().hex
+    name = a_part + "_" + b_part
+    return name
+
+def screenshot():
+    filename = '/tmp/' + mkname() + ".png"
+    subprocess.call(["import", filename])
+    return filename
+
+def main():
+    file = screenshot()
+    print(file)
+
+if __name__ == "__main__":
+    main()
